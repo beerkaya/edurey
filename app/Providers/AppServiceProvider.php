8e7b3893f1d1087b17b8fdd9d6bbc2a884c5e87b
beerkaya <berk-6023@yandex.com>
2022-10-20 01:08:13 +0300

    Author added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Author;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    const VERSION = '0.0.1';
+
     /**
      * Register any application services.
      *
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Author::all()->count() < 10) {
+            Artisan::call('db:seed', ['--class' => 'AuthorSeeder']);
+        }
     }
 }
