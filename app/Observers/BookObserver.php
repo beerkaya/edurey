fd05c554720ba71d38c3341030bfa9176c1448f8
beerkaya <berk-6023@yandex.com>
2022-10-20 01:15:04 +0300

    Book update logging event added

---
+++ app/Observers/BookObserver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Book;
+use App\Models\Log;
+
+class BookObserver
+{
+    /**
+     * Handle the Book "created" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function created(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "updated" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function updated(Book $book)
+    {
+        Log::create([
+            'subject_id' => $book->id,
+            'subject_type' => Book::class,
+            'action' => 'updated',
+            'description' => 'Book updated',
+        ]);
+    }
+
+    /**
+     * Handle the Book "deleted" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function deleted(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "restored" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function restored(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "force deleted" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function forceDeleted(Book $book)
+    {
+        //
+    }
+}
